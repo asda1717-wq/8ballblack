36a99db458e4fef1cf925564cae990646f36b421
asda1717-wq <asda1717@gmail.com>
2026-07-30 22:59:48 -0400

    Add AdvancedControls to MainClient middleware

--- src/eight-ball-client/MainClient.ts
+++ src/eight-ball-client/MainClient.ts
@@ -1,20 +1,18 @@
 import { Middleware } from "polymatic";
-
 import { Terminal } from "./Terminal";
 import { FrameLoop } from "./FrameLoop";
 import { CueShot } from "../eight-ball/CueShot";
 import { RoomClient } from "./RoomClient";
 import { StatusOnline } from "./StatusOnline";
 import { type ClientBilliardContext } from "./ClientContext";
+import { AdvancedControls } from "./AdvancedControls";
 
-/**
- * Main class for the billiard game client.
- */
 export class MainClient extends Middleware<ClientBilliardContext> {
   constructor() {
     super();
     this.use(new FrameLoop());
     this.use(new CueShot());
+    this.use(new AdvancedControls());
     this.use(new Terminal());
     this.use(new RoomClient());
     this.use(new StatusOnline());
